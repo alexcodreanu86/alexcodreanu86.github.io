b6c2b4893648b737188fb21235aa0ee62d940041
Alex Codreanu <alexcodreanu86@gmail.com>
2019-08-25 00:08:16 -0500

    Add my apnea project.

---
+++ static/my-apnea/precache-manifest.e6842383706f7bff3e0b9274862b4854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827382fdcfcb4503ca79e2dc9258103b",
+    "url": "/static/my-apnea/index.html"
+  },
+  {
+    "revision": "4604d8654b74bc267ba7",
+    "url": "/static/my-apnea/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "99153c32677c116c488b",
+    "url": "/static/my-apnea/static/js/2.89964905.chunk.js"
+  },
+  {
+    "revision": "4604d8654b74bc267ba7",
+    "url": "/static/my-apnea/static/js/main.aeb26b9f.chunk.js"
+  },
+  {
+    "revision": "ef759ef35e15cbc5e2c8",
+    "url": "/static/my-apnea/static/js/runtime~main.099cf33b.js"
+  }
+]);
